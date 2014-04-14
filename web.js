e33a4ec2d7e211418db538ee4450f3408988c3e5
Connor Bond <connorbond@outlook.com>
2014-04-14 20:57:36 +0100

    HI

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('OMG! Connor made something and it\'s only gone and worked!');
+  res.send('OMG! Connor made something and it\'s only gone and worked!...');
 });
 
 var port = Number(process.env.PORT || 5000);
